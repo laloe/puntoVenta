52a0f705379b292fee2c29497f8e8b06f18dbfab
Eduardo González <laloe.90@gmail.com>
2017-02-09 18:54:29 -0600

    nuevo producto

--- app/scripts/controllers/productos/modules.js
+++ app/scripts/controllers/productos/modules.js
@@ -3,15 +3,26 @@ angular
   .module('puntoVentaApp')
   .config(function($stateProvider, $urlRouterProvider) {
     var states = [{
-      name: 'main.home.productos',
-      data: {
-        pageTitle: 'PUNTO VENTA | PRODUCTOS'
+        name: 'main.home.productos',
+        data: {
+          pageTitle: 'PUNTO VENTA | PRODUCTOS'
+        },
+        url: '/productos',
+        templateUrl: 'views/productos/productos.html',
+        controller: 'ProductosCtrl',
+        controllerAs: '$ctrl'
       },
-      url: '/productos',
-      templateUrl: 'views/productos/productos.html',
-      controller: 'ProductosCtrl',
-      controllerAs: '$ctrl'
-    }];
+      {
+        name: 'main.home.nuevoProducto',
+        data: {
+          pageTitle: 'PUNTO VENTA | NUEVO'
+        },
+        url: '/nuevoProducto',
+        templateUrl: 'views/productos/nuevoProducto.html',
+        controller: 'NuevoProductoCtrl',
+        controllerAs: '$ctrl'
+      }
+    ];
 
 
     states.forEach(function(state) {
---
+++ app/scripts/controllers/productos/nuevoProducto.js
@@ -0,0 +1,5 @@
+'use strict';
+angular.module('puntoVentaApp')
+  .controller('NuevoProductoCtrl', function(authFactory) {
+    var vm = this;
+  });
